36ca441ad218a2aae5e132e67d1e9d5444dbb432
이성희 <>
2022-05-07 19:58:45 +0900

    용량 계산 수정

--- Assets/Scenes/SampleScene_PatchPage.cs
+++ Assets/Scenes/SampleScene_PatchPage.cs
@@ -40,13 +40,18 @@ public class SampleScene_PatchPage : MonoBehaviour
         }
         else
         {
-            float fileSize = 0.0f;
-            float B2MB = 1f / (1024f * 1024f);
+            long fileSize = 0;
             foreach (var data in needPatchDatas)
-                fileSize += data.fileSize * B2MB;
+                fileSize += data.fileSize;
+
+            float unit_KB = 1024f;
+            float unit_MB = 1024f * 1024f;
+            string fileSizeStr = fileSize > unit_MB ? 
+                string.Format("{0:f2}MB", (fileSize / unit_MB)) : 
+                string.Format("{0:f2}KB", (fileSize / unit_KB));
 
             if (m_ObjPopup != null) m_ObjPopup.SetActive(true);
-            if (m_TextPopup != null) m_TextPopup.text = $"Download Popup\nFile Count: {needPatchDatas.Count()}\nFile Size: {fileSize.ToString("f2")}MB";
+            if (m_TextPopup != null) m_TextPopup.text = $"Download Popup\nFile Count: {needPatchDatas.Count()}\nFile Size: {fileSizeStr}";
             m_IsReadyToPopup = true;
         }
         while(m_IsReadyToPopup == true) yield return null;
